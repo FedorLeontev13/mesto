5a53410fd5feb7b32deadef07449b49eb3bab8e1
Fedor Leontev <fedorleontev13@gmail.com>
2020-04-15 14:57:12 +0300

    Project 3 (3)

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const btnEdit = document.getElementById('btnEdit')
 
 let popup = document.querySelector('.popup')
 let container = document.querySelector('.popup__container')
-let btnSave = document.querySelector('.popup_btn_save')
-let nameInput = document.querySelector('.popup__text_name')
-let jobInput = document.querySelector('.popup__text_description')
-let nameForm = document.querySelector('.profile__title')
-let jobForm = document.querySelector('.profile__subtitle')
+let btnSave = document.querySelector('.popup__btn-save')
+//let nameInput = document.querySelector('.popup__text_name')       // Если оставить переменные тут, то скрипт ломается и после второго "сохранения" данные стираются.
+//let jobInput = document.querySelector('.popup__text_description')
+//let nameForm = document.querySelector('.profile__title')
+//let jobForm = document.querySelector('.profile__subtitle')
 
 
 function open() {
-  popup.classList.toggle('popup_opened')
+  popup.classList.add('popup_opened')
 }
 
 function close() {
-  popup.classList.toggle('popup_opened', false)
+  popup.classList.remove('popup_opened')
 }
 
 btnEdit.addEventListener('click', open)
@@ -23,11 +23,17 @@ btnClose.addEventListener('click', close)
 
 
 
-function formSubmitHandler (evt) {
+function formSubmitHandler (evt) {          // Данная функция выполена по примеру из указаний к задаче, полностью скопирована (как там и сказано сделать)
     evt.preventDefault()
+
+    let nameInput = document.querySelector('.popup__text_name') // создание и вызов переменных по примеру происходит внутри тела функции
+    let jobInput = document.querySelector('.popup__text_description') 
 
     nameInput = nameInput.value
     jobInput = jobInput.value
+
+    let nameForm = document.querySelector('.profile__title') 
+    let jobForm = document.querySelector('.profile__subtitle')
 
     nameForm.textContent = nameInput
     jobForm.textContent = jobInput
@@ -36,8 +42,7 @@ function formSubmitHandler (evt) {
 container.addEventListener('submit', formSubmitHandler);
 
 function save() {
-  popup.classList.toggle('popup_opened', false)
+  popup.classList.remove('popup_opened')
 }
 
 btnSave.addEventListener('click', save)
-
